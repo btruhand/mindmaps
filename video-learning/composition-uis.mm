b513925e99a06b8910f28c53bab6e7f61c181f3d
Btara Truhandarien <10100624+btruhand@users.noreply.github.com>
2020-09-18 00:28:59 -0400

    add mindmap on Compositional UI video

---
+++ video-learning/composition-uis.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1600400357214" ID="ID_113573286" LINK="https://youtu.be/gjtFGx0yX5M?t=1897" MODIFIED="1600400966908" TEXT="Compositional UIs">
+<node CREATED="1600400911779" ID="ID_244336950" MODIFIED="1600401003850" POSITION="right" TEXT="Serve required data for the front in one go">
+<node CREATED="1600401004382" ID="ID_77768755" MODIFIED="1600401009160" TEXT="What does it mean?"/>
+<node CREATED="1600401009473" ID="ID_1269511925" MODIFIED="1600401065265" TEXT="It means that we may want to get as much data as we need in small # of requests"/>
+<node CREATED="1600401069125" ID="ID_1313848887" MODIFIED="1600401079709" TEXT="Potentially LAAAARGE responses"/>
+<node CREATED="1600401085764" ID="ID_1662491089" MODIFIED="1600401824474" TEXT="&quot;Compose the model&quot;">
+<node CREATED="1600401099530" ID="ID_241560734" MODIFIED="1600401683788" TEXT="Aggregate disparate results into one"/>
+<node CREATED="1600401828076" ID="ID_660611408" MODIFIED="1600401837861" TEXT="Find where all the required data comes from">
+<node CREATED="1600402429489" ID="ID_1113929750" MODIFIED="1600402448314" TEXT="Coordination!"/>
+<node CREATED="1600402544462" ID="ID_594059764" MODIFIED="1600402553036" TEXT="May lead to a lot of hardcoding of information"/>
+<node CREATED="1600402554279" ID="ID_1051663495" MODIFIED="1600402570636" TEXT="The ideal situation is one change in the downstream doesn&apos;t change the upstream (that much)"/>
+<node CREATED="1600402580798" ID="ID_385803391" MODIFIED="1600402593994" TEXT="The interesting part from Amazon: REQUEST THEM ALL">
+<node CREATED="1600402614608" ID="ID_1947743153" MODIFIED="1600402617647" TEXT="Route smartly"/>
+<node CREATED="1600402650702" ID="ID_267566089" MODIFIED="1600402660164" TEXT="Example was using interface for generic filtering"/>
+</node>
+<node CREATED="1600402702547" ID="ID_1910310434" MODIFIED="1600402713146" TEXT="Keep appending data">
+<node CREATED="1600402713146" ID="ID_111356170" MODIFIED="1600402722777" TEXT="Leads to dynamic but difficult to understand data"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
